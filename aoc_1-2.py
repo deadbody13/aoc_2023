193f24f84b4db53d5c0a3d676e9fd20dd1036f78
deadbody13 <57448148+deadbody13@users.noreply.github.com>
2023-12-01 15:25:24 -0500

    Add files via upload

---
+++ aoc_1-2.py
@@ -0,0 +1,64 @@
+def spelled_to_num(spelled_string):
+    words =     ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    integers =  ["1",   "2",   "3",     "4",    "5",    "6",   "7",     "8",     "9"]
+    # partial starts off as full string and is checked to see if it starts with a spelled-out number
+    partial = spelled_string
+    for x in range(len(spelled_string)):
+        for y in range(len(words)):
+            # check if partial starts with spelled-out word
+            if (partial.startswith(words[y])):
+                # replace only the first occurrence of that word in the original string
+                spelled_string = spelled_string.replace(words[y], integers[y], 1)
+        # trim off leading letter from partial
+        partial = partial[1:] 
+    return spelled_string.split()
+
+# put data into contents_spelled[], contains numbers spelled out
+file = open("input_1-2.txt", "r")
+contents_spelled = []
+for x in file:
+    contents_spelled += x.split()
+file.close()
+
+# # test data
+# # answer: 281
+# contents_spelled = [
+#     "two1nine",
+#     "eightwothree",
+#     "abcone2threexyz",
+#     "xtwone3four",
+#     "4nineeightseven2",
+#     "zoneight234",
+#     "7pqrstsixteen"
+# ]
+
+# replace spelled numbers, put into contents[]
+contents = []
+for x in contents_spelled:
+    contents += spelled_to_num(x)
+
+# print replaced strings to output_1-2.txt
+file = open("output_1-2.txt", "w")
+for x in contents:
+    file.write(x,)
+    file.write("\n")
+file.close()
+
+
+# get numbers from strings and add to num_list[]
+num_list = []
+for x in contents:
+    # add all numbers to str(number)
+    num= ""
+    for y in x:
+        if y.isdigit():
+            num+= y
+    # concatenate first and last number
+    num= num[0] + num[-1]
+    num_list.append(int(num))
+
+# add numbers together to get int(num_sum)
+num_sum = 0
+for x in num_list:
+    num_sum += x
+print(num_sum)
